65f63ca248ca865179535e98e6cd92d140c7e448
Felix Saaro <felix@saaro.ch>
2021-11-26 17:45:59 +0100

    Fix time

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ const MyApp = ({ Component, pageProps }) => (
     <div id="root">
         <Head>
             <title>ZHAW | Beer Pong</title>
-            <meta name="description" content="Am 3.12 ab 18.00 in der T&uuml;rmlibar. Jetzt anmelden, mitmachen und gewinnen! 10.- pro Team inklusive Bier." />
+            <meta name="description" content="Am 3.12 ab 19.00 in der T&uuml;rmlibar. Jetzt anmelden, mitmachen und gewinnen! 10.- pro Team inklusive Bier." />
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -15,7 +15,7 @@ const MyApp = ({ Component, pageProps }) => (
             <meta name="theme-color" content="#ffffff" />
             <meta property="og:image:width" content="279" />
             <meta property="og:image:height" content="279" />
-            <meta property="og:description" content="Am 3.12 ab 18.00 in der T&uuml;rmlibar. Jetzt anmelden, mitmachen und gewinnen! 10.- pro Team inklusive Bier." />
+            <meta property="og:description" content="Am 3.12 ab 19.00 in der T&uuml;rmlibar. Jetzt anmelden, mitmachen und gewinnen! 10.- pro Team inklusive Bier." />
             <meta property="og:title" content="Beer Pong Turnier" />
             <meta property="og:url" content="https://zhaw-beer-pong.ch" />
             <meta property="og:image" content="https://zhaw-beer-pong.ch/og-image.jpg/og-image.jpg" />
